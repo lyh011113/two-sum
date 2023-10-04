b190b4334594c07bfb429a469b84528f1f4b8b02
lyh011113 <john1111369@g.skku.edu>
2023-10-04 09:56:14 +0000

    submit Assignment main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+# YOUR ANSWER
+    n = len(nums)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+
